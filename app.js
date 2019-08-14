bc22cf6f64094bb06e2646efc23591ad7bc14025
Min-92 <0201.mj.lee@gmail.com>
2019-08-15 01:20:57 +0900

    feat : mongoose connect

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/heritage', indexRouter);
+app.use('/heritage', heritageRouter);
 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
@@ -29,14 +30,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
+    res.status(err.status || 500).send();
 })
-
-mongoose.connect((mongoURI), {
-    useCreateIndex: true,
-    useNewUrlParser: true
-});
 
 mongoose.connect((mongoURI), {
     useCreateIndex: true,
