cae6fe80931d009caeef6f76e70fce9fcb073841
Min-92 <0201.mj.lee@gmail.com>
2019-08-15 01:19:58 +0900

    feat : index router 추가

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { Heritage } = require('../model/database');
+
+const router = express.Router();
+
+const categoryObj = {
+    ios: 'ios', front: 'front', back: 'back'
+}
+
+router.get('/', async (req, res, next) => {
+    res.sendStatus(200);
+})
+
+router.get('/:category', async (req, res, next) => {
+    const category = req.params.category;
+    if (categoryObj[category]) {
+        const heritageList = await Heritage.find({ category });
+        return res.send({ heritageList });
+    }
+    return next();
+})
+
+module.exports = router;
