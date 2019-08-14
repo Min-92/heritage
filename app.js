a035f32afa380108f9abda61076b00b686b51076
Min-92 <0201.mj.lee@gmail.com>
2019-08-14 22:18:14 +0900

    feat : cors 추가

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
