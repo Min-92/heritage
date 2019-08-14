70bf9ac32cf363806650e254353dd95ce683199b
Min-92 <0201.mj.lee@gmail.com>
2019-08-14 22:13:11 +0900

    feat : app.js 추가

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+})
+
+mongoose.connect((mongoURI), {
+    useCreateIndex: true,
+    useNewUrlParser: true
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
