f155c35fafcbc526dcd8b46c6b0ddbb3decdc19c
Min-92 <0201.mj.lee@gmail.com>
2019-08-14 22:13:41 +0900

    feat : loger 추가

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const logger = require('morgan');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
