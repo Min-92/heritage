54c7f66bb57b6a3748416e2dd0f76adf8385cdfa
Min-92 <0201.mj.lee@gmail.com>
2019-08-15 01:20:14 +0900

    feat : heritage router 추가

--- routes/heritage.js
+++ routes/heritage.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const { Heritage } = require('../model/database');
 
 const router = express.Router();
 
-router.get('/', (req,res,next) => {
+router.post('/', async (req, res, next) => {
+    try {
+        const { questions, category, company } = req.body;
+        for (let i in questions) {
+            const heritage = new Heritage({
+                question: questions[i],
+                category,
+                company
+            })
+
+            heritage.save();
+        }
+        return res.sendStatus(200);
+    } catch (err) {
+        return next(err);
+    }
+
 })
 
 module.exports = router;
