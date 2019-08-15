ed6da473c506f05da56eba0ebe4acc4e65e92d94
Min-92 <0201.mj.lee@gmail.com>
2019-08-15 11:42:40 +0900

    refactor : Schema 수정

--- model/database.js
+++ model/database.js
@@ -4,7 +4,8 @@ const db = mongoose.connection;
 const heritageSchema = new mongoose.Schema({
     category: { type: String, required: true },
     question: { type: String, required: true },
-    company: { type: String, required: true }
+    company: { type: String, required: true },
+    deleted: {type : Boolean, default: false}
 }, { timestamps: { createdAt: 'created_at' } })
 
 const Heritage = new mongoose.model('Heritage', heritageSchema);
